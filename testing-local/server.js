a536b8d1034b5c4afc8733b248f4c0134dc24ace
NiloJr-sun <nilo.castillano@sun-asterisk.com>
2025-04-11 18:28:12 +0800

    Can now only paste release link + thread time stamp

--- testing-local/server.js
+++ testing-local/server.js
@@ -7,20 +7,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/check-release", (req, res) => {
   const text = req.body.text; // e.g., "1234 my-org/my-repo"
-  const [PR_NUMBER, REPO] = text.split(" ");
-  if (!PR_NUMBER || !REPO) {
+  const [PR_LINK, THREAD_TS] = text.split(" ");
+  if (!PR_LINK) {
     return res.send("Usage: /check-release [PR_NUMBER] [REPO]");
   }
 
-  execFile("./check-release.sh", [PR_NUMBER, REPO], (error, stdout, stderr) => {
-    if (error) {
-      console.error(stderr);
-      return res.status(500).send("Error running script.");
+  execFile(
+    "./check-release.sh",
+    [PR_LINK, THREAD_TS],
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(stderr);
+        return res.status(500).send("Error running script.");
+      }
+      res.send(
+        `Checking release... Slack should get the update shortly. ${text}`
+      );
     }
-    res.send(
-      `Checking release... Slack should get the update shortly. ${text}`
-    );
-  });
+  );
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
